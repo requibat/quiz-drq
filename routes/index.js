8930959018dc16ae3bcc206cb365f7b9c0ce1493
requibat <requibat@gmail.com>
2015-06-04 18:27:08 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
+router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer',quizController.answer);
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+
+
 
 module.exports = router;
