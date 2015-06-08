608aff5f22dfd085a3ac43dcb4a373c6ab72d257
requibat <requibat@gmail.com>
2015-06-08 20:34:49 +0200

    Instalado Load

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,18 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
+//pagina entrada
+router.get('/', function(req,res){
+	res.render('index', {title: 'Quiz' });
+});
+
+router.param('quizId', quizController.load);
+
+//Rutas /quizes
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/author',quizController.author);
-
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
-});
-
 
 
 
