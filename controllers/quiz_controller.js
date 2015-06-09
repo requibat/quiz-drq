5ef4e238649d950eda49f3d291ced29725d125f7
requibat <requibat@gmail.com>
2015-06-09 13:16:28 +0200

    Incluidas busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,10 +14,17 @@ exports.load=function(req, res, next,quizId){
 
 // Get /quizes
 exports.index = function(req,res) {
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});
+	if (!req.query.search){ 
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index.ejs', {quizes: quizes});
+		}
+		).catch(function(error) {next(error);})
+	}else{
+		models.Quiz.findAll({where: ["pregunta like ?","%"+req.query.search.split(" ").join("%")+"%"]} ).then(function(quizes){
+			res.render('quizes/index.ejs', {quizes: quizes.sort()});
+		}
+		).catch(function(error) {next(error);})
 	}
-	).catch(function(error) {next(error);})
 };
 
 //Get /quiz/:id
